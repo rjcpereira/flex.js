b9648ae8b6b9a1ca4c1d2961aae47abec757922f
Ricardo Pereira <rjcpereira@outlook.pt>
2022-04-13 17:14:38 +0100

    5

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
+const fs = require('fs'),
+    gulp = require('gulp'),
+    rename = require('gulp-rename'),
+    colors = require('colors/safe'),
+    colors = require('colors/safe');
 
-console.log("running");
+const copyResources = () => {
+
+};
+
+!fs.existsSync('flex.js') && copyResources();
+/* 
+console.log(fs.existsSync('dev.js')) */
+
+/* gulp.src('node/dev.js')
+    .pipe(rename('cenas.js'))
+    .pipe(gulp.dest('toma-la')) */
 
 module.exports = () => {
     console.log(colors.green("hello"));
