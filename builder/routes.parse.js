b653ca8fa4aa06b936e121fce4b185994debb301
Ricardo Pereira <rjcpereira@outlook.pt>
2022-04-14 19:05:52 +0100

    v20

--- builder/routes.parse.js
+++ builder/routes.parse.js
@@ -1,1 +1,4 @@
-module.exports = ({ next }) => next();
+const fs = require('fs'),
+shell = require('child_process').exec;
+
+module.exports = ({ next }) => shell(`handlebars layouts/base.hbs -f ./dist/scripts/templates.js`, next);
