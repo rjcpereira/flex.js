0f1f1463c51ade3e00970b6399a6ecdaafcdd159
Ricardo Pereira <rjcpereira@outlook.pt>
2022-04-15 10:18:48 +0100

    test article

--- builder/routes.parse.js
+++ builder/routes.parse.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const { log, folders } = require('./utils');
 
 module.exports = ({ next, layouts, compile }) => {
@@ -6,11 +7,39 @@ module.exports = ({ next, layouts, compile }) => {
 
     const render = compile(layouts.base.file);
 
-    console.log(render({
-        title: 'Title',
-        description: 'Description'
-    }))
+    const main = {
+        '@route': {
+            path: '',
+            params: {}
+        },
+        '@url': 'http://localhost:3000'
+    };
     
+    const body = render({
+        ...main,
+        title: 'Title',
+        description: 'Description',
+        menu: {
+            items: [
+                { url: '/', title: 'Home' },
+                { url: '/about', title: 'About' }
+            ]
+        },
+        highlights: {
+            world: [
+                { title: 'First World Article', body: '<span style="font-style:italic;">First world article content</span>' }
+            ],
+            tech: [
+                { title: 'Last Tech Article', body: 'Last tect article content' },
+                { title: 'First Tech Article', body: 'First tect article content <pre>with the pre element</pre>' }
+            ]
+        }
+    });
+
+    const html = `<!DOCTYPE html><html><head></head><body>${body}</body></html>`;
+    
+    fs.writeFileSync('dist/web/index.html', html);
+
     //folders('pages', (item, path) => console.log(item, path))
 
     next();
--- core/server.js
+++ core/server.js
@@ -4,12 +4,12 @@ const server = express();
 
 const port = 3000;
 
-const use = (req, res, next) => server.use(req, res, next);
+//const use = (req, res, next) => server.use(req, res, next);
 
-server.use((req, res) => res.send({
+server.get('/dev', (req, res) => res.send({
     id: 'flex.js'
 }));
 
-server.use(express.static('/dist'));
+server.use(express.static('dist/web'));
 
 server.listen(port, () => console.log(`listening on port ${port}`));
